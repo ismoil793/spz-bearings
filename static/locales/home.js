e237a6572fad2cc730f25879b254d757fca981bc
ismoil793 <ismoil.793@gmail.com>
2022-01-11 01:33:29 +0500

    language correction

--- static/locales/home.js
+++ static/locales/home.js
@@ -1,200 +1,197 @@
 const homeText = {
-  ru: {
-    pageTitle: 'Ахунбабаевский подшипниковый завод',
-    video_section_title: 'Галлерея',
-    slider: [{
-        title: "У нас большой опыт",
-        description: "Завод на рынке более 35 лет - функционирует с 1986-года.",
-        button: 'О Нас'
+   ru: {
+      pageTitle: 'Ахунбабаевский подшипниковый завод',
+      video_section_title: 'Галлерея',
+      slider: [{
+         title: "У нас большой опыт",
+         description: "Завод на рынке более 35 лет - функционирует с 1986-года.",
+         button: 'О Нас'
       },
-      {
-        title: "Качество и доступность",
-        description: "Изготовим подшипники любой сложности, качество будет соответсвующим к ГОСТ, цена обрадует Вас.",
-        button: 'Подробнее'
-      },
-      {
-        title: "Поможем с консультацией",
-        description: "Мы можем оказать вам любую консультацию по приобретению.",
-        button: 'Контакты'
-      },
-    ],
-    benefits: {
-      title: 'Наши преимущества',
-      descriptions: ['Только качественные комплектующие', 'Сочетание высокого качества, надёжности и обоснованной цены', 'Полный технологический цикл']
-    },
-    product_demand: {
-      mainTitle: "Продукция восстребована в секторах",
-      cards: [{
-          title: 'Железнодорожная-промышленность',
-          details: ["Bearings","Shafts","Gears","Nuts","Bolts"]
-        },
-        {
-          title: 'Авто-промышленность',
-          details: ["Bearings","Shafts","Gears","Nuts","Bolts"]
-        },
-        {
-          title: 'Энергетика',
-          details: ["Bearings","Shafts","Gears","Nuts","Bolts"]
-        },
-        {
-          title: 'Тяжелая промышленность',
-          details: ["Bearings","Shafts","Gears","Nuts","Bolts"]
-        }
+         {
+            title: "Качество и доступность",
+            description: "Изготовим подшипники любой сложности, качество будет соответсвующим к ГОСТ, цена обрадует Вас.",
+            button: 'Подробнее'
+         },
+         {
+            title: "Поможем с консультацией",
+            description: "Мы можем оказать вам любую консультацию по приобретению.",
+            button: 'Контакты'
+         },
       ],
-      button: "Продукты"
-    },
-    redirect_card: {
-      title: "Наша продукция восстребована и в других секторах.",
-      button: "Узнать больше"
-    },
-    technologies_section: {
-      sectionTitle: 'МЫ ИСПОЛЬЗУЕМ САМЫЕ СОВРЕМЕННЫЕ ТЕХНОЛОГИИ',
-      title: 'Мы в погоне за иновациями',
-      description: 'Для поддержания лидерских позиций на рынке мы постоянно стремимся к совершенству так и в технолгиях, так и в ассортименте продукции.',
-      button: 'Подробнее'
-    },
-    news_section: {
-      sectionTitle: 'Новости',
-      title: "Последние новости", 
-      news: [
-        {title: 'Расширение', description: "Мы выпускаем ещё одну линейку продуктов"},
-        {title: 'Деятельность', description: "Наш завод выполнил крупный заказ для Компании...."}
-      ]
-    }
-  },
-
-
-
-
-  uz: {
-    pageTitle: "Ohunboboev Podshipnik Zavodi",
-    video_section_title: 'Galereya',
-    slider: [{
-        title: "Bizda katta tajriba bor",
-        description: "Zavod 35 yildan ortiq bozorda - 1986 yildan beri faoliyat olib boradi",
-        button: 'Biz Haqimizda'
+      benefits: {
+         title: 'Наши преимущества',
+         descriptions: ['Только качественные комплектующие', 'Сочетание высокого качества, надёжности и обоснованной цены', 'Полный технологический цикл']
       },
-      {
-        title: "Sifat va hamyonboplik",
-        description: "Biz har qanday murakkablikdagi podshipniklar ishlab chiqaramiz, sifati GOSTga mos keladi, narx sizga yoqadi.",
-        button: 'Batafsil ma\'lumot'
+      product_demand: {
+         mainTitle: "Продукция восстребована в секторах",
+         cards: [{
+            title: 'Железнодорожная-промышленность',
+            details: ["Подшипники", "Валы", "Шестерни", "Гайки", "Болты"]
+         },
+            {
+               title: 'Авто-промышленность',
+               details: ["Подшипники", "Валы", "Шестерни", "Гайки", "Болты"]
+            },
+            {
+               title: 'Энергетика',
+               details: ["Подшипники", "Валы", "Шестерни", "Гайки", "Болты"]
+            },
+            {
+               title: 'Тяжелая промышленность',
+               details: ["Подшипники", "Валы", "Шестерни", "Гайки", "Болты"]
+            }
+         ],
+         button: "Продукты"
       },
-      {
-        title: "Maslahat bilan yordamlashamiz",
-        description: "Sizga sotib olish bo'yicha maslahat bera olamiz.",
-        button: 'Kontaktlar'
+      redirect_card: {
+         title: "Наша продукция восстребована и в других секторах.",
+         button: "Узнать больше"
       },
-    ],
-    benefits: {
-      title: 'Bizning ustunliklarimiz',
-      descriptions: ['Faqatgina sifatli mahsulotdan foydalanamiz', "Yuqori sifat ishonchlilik va maqbul narh uyg'unligi", "To'liq tehnologik tsikl bilan ta'minlashga qodirmiz"]
-    },
-    product_demand: {
-      mainTitle: "Maxsulotlarimizga talab katta bo'lgan sektorlar",
-      cards: [{
-          title: "Temir-yo'l sanoati",
-          details: ["Podshipniklar", "Vallar", "SHesternyalar", "Gaykalar", "Boltlar"]
-        },
-        {
-          title: 'Avto sanoati',
-          details: ["Podshipniklar", "Vallar", "SHesternyalar", "Gaykalar", "Boltlar"]
-        },
-        {
-          title: 'Energetika',
-          details: ["Podshipniklar", "Vallar", "SHesternyalar", "Gaykalar", "Boltlar"]
-        },
-        {
-          title: "Og'ir sanoati",
-          details: ["Podshipniklar", "Vallar", "SHesternyalar", "Gaykalar", "Boltlar"]
-        }
+      technologies_section: {
+         sectionTitle: 'МЫ ИСПОЛЬЗУЕМ САМЫЕ СОВРЕМЕННЫЕ ТЕХНОЛОГИИ',
+         title: 'Мы в погоне за иновациями',
+         description: 'Для поддержания лидерских позиций на рынке мы постоянно стремимся к совершенству так и в технолгиях, так и в ассортименте продукции.',
+         button: 'Подробнее'
+      },
+      news_section: {
+         sectionTitle: 'Новости',
+         title: "Последние новости",
+         news: [
+            {title: 'Расширение', description: "Мы выпускаем ещё одну линейку продуктов"},
+            {title: 'Деятельность', description: "Наш завод выполнил крупный заказ для Компании...."}
+         ]
+      }
+   },
+
+
+   uz: {
+      pageTitle: "Ohunboboev Podshipnik Zavodi",
+      video_section_title: 'Galereya',
+      slider: [{
+         title: "Bizda katta tajriba bor",
+         description: "Zavod 35 yildan ortiq bozorda - 1986 yildan beri faoliyat olib boradi",
+         button: 'Biz Haqimizda'
+      },
+         {
+            title: "Sifat va hamyonboplik",
+            description: "Biz har qanday murakkablikdagi podshipniklar ishlab chiqaramiz, sifati GOSTga mos keladi, narx sizga yoqadi.",
+            button: 'Batafsil ma\'lumot'
+         },
+         {
+            title: "Maslahat bilan yordamlashamiz",
+            description: "Sizga sotib olish bo'yicha maslahat bera olamiz.",
+            button: 'Kontaktlar'
+         },
       ],
-      button: 'Mahsulotlar'
-    },
-    redirect_card: {
-      title: "Mahsulotlarimizga boshqa sektorlardayam talab bor",
-      button: "Batafsil ma'lumot olish"
-    },
-    technologies_section: {
-      sectionTitle: 'Biz eng zamonaviy tehnologiyalardan foydalanamiz',
-      title: 'Biz yangi innovatsiyalar poygasidamiz',
-      description: 'Bozordagi yetakchi mavqeimizni saqlab qolish maqsadida doimo ham tekhnologiyalarda, ham mahsulot asortimentida mukamallikka intilamiz.',
-      button: 'Batafsil ma"lumot'
-    },
-    news_section: {
-      sectionTitle: 'Habarlar',
-      title: "So'nggi habarlar", 
-      news: [
-        {title: 'Kengayish', description: "Biz ishlab chiqaradigan mahsulot asortimentini kengaytirdik"},
-        {title: 'Faoliyat', description: "Bizning Zavod katta hajmli zakazni amalga oshirdi..."}
-      ]
-    }
-  },
+      benefits: {
+         title: 'Bizning ustunliklarimiz',
+         descriptions: ['Faqatgina sifatli mahsulotdan foydalanamiz', "Yuqori sifat ishonchlilik va maqbul narh uyg'unligi", "To'liq tehnologik tsikl bilan ta'minlashga qodirmiz"]
+      },
+      product_demand: {
+         mainTitle: "Maxsulotlarimizga talab katta bo'lgan sektorlar",
+         cards: [{
+            title: "Temir-yo'l sanoati",
+            details: ["Podshipniklar", "Vallar", "SHesternyalar", "Gaykalar", "Boltlar"]
+         },
+            {
+               title: 'Avto sanoati',
+               details: ["Podshipniklar", "Vallar", "SHesternyalar", "Gaykalar", "Boltlar"]
+            },
+            {
+               title: 'Energetika',
+               details: ["Podshipniklar", "Vallar", "SHesternyalar", "Gaykalar", "Boltlar"]
+            },
+            {
+               title: "Og'ir sanoati",
+               details: ["Podshipniklar", "Vallar", "SHesternyalar", "Gaykalar", "Boltlar"]
+            }
+         ],
+         button: 'Mahsulotlar'
+      },
+      redirect_card: {
+         title: "Mahsulotlarimizga boshqa sektorlardayam talab bor",
+         button: "Batafsil ma'lumot olish"
+      },
+      technologies_section: {
+         sectionTitle: 'Biz eng zamonaviy tehnologiyalardan foydalanamiz',
+         title: 'Biz yangi innovatsiyalar poygasidamiz',
+         description: 'Bozordagi yetakchi mavqeimizni saqlab qolish maqsadida doimo ham tekhnologiyalarda, ham mahsulot asortimentida mukamallikka intilamiz.',
+         button: 'Batafsil ma"lumot'
+      },
+      news_section: {
+         sectionTitle: 'Habarlar',
+         title: "So'nggi habarlar",
+         news: [
+            {title: 'Kengayish', description: "Biz ishlab chiqaradigan mahsulot asortimentini kengaytirdik"},
+            {title: 'Faoliyat', description: "Bizning Zavod katta hajmli zakazni amalga oshirdi..."}
+         ]
+      }
+   },
 
 
-  en: {
-    pageTitle: 'Akhunbabaevsk Bearing Plant',
-    video_section_title: 'Gallery',
-    slider: [{
-        title: "We have a lot of experience",
-        description: "The plant has been on the market for over 35 years - it has been operating since 1986.",
-        button: 'About us'
+   en: {
+      pageTitle: 'Akhunbabaevsk Bearing Plant',
+      video_section_title: 'Gallery',
+      slider: [{
+         title: "We have a lot of experience",
+         description: "The plant has been on the market for over 35 years - it has been operating since 1986.",
+         button: 'About us'
       },
-      {
-        title: "Quality and availability",
-        description: "We will produce bearings of any complexity, the quality will correspond to quality standards, the price will please you.",
-        button: 'More details'
-      },
-      {
-        title: "We will help with purchase advice",
-        description: "We can provide you with any purchase advice.",
-        button: 'Contacts'
-      },
-    ],
-    benefits: {
-      title: 'Our advantages',
-      descriptions: ['We use only high quality components for production','Combination of high quality, reliability and reasonable price','We can provide a full technological production cycle']
-    },
-    product_demand: {
-      mainTitle: "Products are in demand in sectors",
-      cards: [{
-          title: 'Railway industry',
-          details: ["Bearings","Shafts","Gears","Nuts","Bolts"]
-        },
-        {
-          title: 'Auto industry',
-          details: ["Bearings","Shafts","Gears","Nuts","Bolts"]
-        },
-        {
-          title: 'Energy Industry',
-          details: ["Bearings","Shafts","Gears","Nuts","Bolts"]
-        },
-        {
-          title: 'Heavy industry',
-          details: ["Bearings","Shafts","Gears","Nuts","Bolts"]
-        }
+         {
+            title: "Quality and availability",
+            description: "We will produce bearings of any complexity, the quality will correspond to quality standards, the price will please you.",
+            button: 'More details'
+         },
+         {
+            title: "We will help with purchase advice",
+            description: "We can provide you with any purchase advice.",
+            button: 'Contacts'
+         },
       ],
-      button: "Products"
-    },
-    redirect_card: {
-      title: "Our products are in demand in other sectors as well.",
-      button: "More about it"
-    },
-    technologies_section: {
-      sectionTitle: 'WE USE THE MOST MODERN TECHNOLOGIES',
-      title: "We are in pursuit of innovation",
-      description: 'To maintain our leadership position in the market, we are constantly striving for excellence in technology and the range of products.',
-      button: 'More about it'
-    },
-    news_section: {
-      sectionTitle: 'News',
-      title: "Last News", 
-      news: [
-        {title: 'Expansion', description: "We release another product line"},
-        {title: 'Activity', description: "Our plant has completed a large order for the Company...."}
-      ]
-    }
-  },
-
+      benefits: {
+         title: 'Our advantages',
+         descriptions: ['We use only high quality components for production', 'Combination of high quality, reliability and reasonable price', 'We can provide a full technological production cycle']
+      },
+      product_demand: {
+         mainTitle: "Products are in demand in sectors",
+         cards: [{
+            title: 'Railway industry',
+            details: ["Bearings", "Shafts", "Gears", "Nuts", "Bolts"]
+         },
+            {
+               title: 'Auto industry',
+               details: ["Bearings", "Shafts", "Gears", "Nuts", "Bolts"]
+            },
+            {
+               title: 'Energy Industry',
+               details: ["Bearings", "Shafts", "Gears", "Nuts", "Bolts"]
+            },
+            {
+               title: 'Heavy industry',
+               details: ["Bearings", "Shafts", "Gears", "Nuts", "Bolts"]
+            }
+         ],
+         button: "Products"
+      },
+      redirect_card: {
+         title: "Our products are in demand in other sectors as well.",
+         button: "More about it"
+      },
+      technologies_section: {
+         sectionTitle: 'WE USE THE MOST MODERN TECHNOLOGIES',
+         title: "We are in pursuit of innovation",
+         description: 'To maintain our leadership position in the market, we are constantly striving for excellence in technology and the range of products.',
+         button: 'More about it'
+      },
+      news_section: {
+         sectionTitle: 'News',
+         title: "Last News",
+         news: [
+            {title: 'Expansion', description: "We release another product line"},
+            {title: 'Activity', description: "Our plant has completed a large order for the Company...."}
+         ]
+      }
+   },
 
 
 }
