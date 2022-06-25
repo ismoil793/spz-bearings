2424c02350fee416cff69a1a614f308594233003
ismoil793 <ismoil.793@gmail.com>
2022-06-25 13:51:13 +0500

    fixed product API

--- store/actions/product.js
+++ store/actions/product.js
@@ -1,16 +1,29 @@
 import {
-  API_getProduct,
+   API_getProduct,
 } from "../../api-spz/requests/product";
 import * as action from "../types/actionTypes";
 import logRequestError from "./errorHandler";
 
 export const fetchProducts = (subCategoryID) => async (dispatch) => {
-  await API_getProduct(subCategoryID)
-    .then((res) => {
-      dispatch({
-        type: action.FETCH_PRODUCTS,
-        payload: res.data.data,
-      });
-    })
-    .catch((e) => logRequestError(e));
+   let products = [];
+
+   await API_getProduct(subCategoryID, {per_page: 1000})
+       .then(async (res) => {
+          const {current_page, last_page, data} = res.data || {};
+          products = Array.isArray(data) ? [...data] : products;
+
+          if (current_page < last_page) {
+             await API_getProduct(subCategoryID, {page: current_page + 1}).then(
+                 (res) => {
+                    const {data} = res.data || {};
+                    products = Array.isArray(data) ? [...products, ...data] : products;
+                    dispatch({
+                       type: action.FETCH_PRODUCTS,
+                       payload: products,
+                    });
+                 }
+             );
+          }
+       })
+       .catch((e) => logRequestError(e));
 };
